81a39a0789d41a127bc7f59ecd8b0c8a64848592
Aneesh Arnav <80468784+aneesharnavch@users.noreply.github.com>
2025-07-29 19:04:16 +0530

    Add files via upload

---
+++ examples/Outliers/IsMADOutlier.ino
@@ -0,0 +1,18 @@
+#include <microdsp.h>
+
+MicroDSP dsp;
+
+void setup() {
+  Serial.begin(9600);
+
+  float data[] = {1.0, 2.0, 3.0, 4.0, 5.0, 100.0};
+  size_t len = sizeof(data) / sizeof(data[0]);
+
+  bool isOutlier = dsp.isMADOutlier(100.0, data, len);
+
+  Serial.print("Is 100.0 a MAD outlier? ");
+  Serial.println(isOutlier ? "Yes" : "No");
+}
+
+void loop() {
+}
